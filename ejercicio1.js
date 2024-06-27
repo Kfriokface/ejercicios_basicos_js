3569342b93c53b4cfce11f1aed11a50fd46a1210
Alberto Sancho <designalbertosancho@gmail.com>
2024-06-27 20:08:27 +0200

    ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,8 @@
+const miSuperheroeFavorito = "Hulk";
+const numeroFavorito = 50;
+const altura = 5;
+const longitud = 10;
+
+let suma = altura + longitud;
+
+console.log(miSuperheroeFavorito, '\n', numeroFavorito, '\n', altura, '\n', longitud,'\n',  suma)
