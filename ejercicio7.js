9c3cded543fd7e016d0382b949a4257a891da658
Alberto Sancho <designalbertosancho@gmail.com>
2024-06-28 23:23:14 +0200

    ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,13 @@
+function greaterNumber(numberOne, numberTwo) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne);
+  }
+  else if ( numberOne < numberTwo){
+    console.log(numberTwo);
+  }
+  else {
+    console.log("Los dos números son iguales");
+  }
+}
+
+greaterNumber(54, 107);
