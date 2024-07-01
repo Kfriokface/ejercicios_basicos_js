9fc90f72755b860d70438815114bfdc58f56db1e
Alberto Sancho <designalbertosancho@gmail.com>
2024-07-02 00:37:10 +0200

    ejercicio 13

---
+++ ejercicio13.js
@@ -0,0 +1,26 @@
+const names = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function nameFinder(nameList, name) {
+    const posicion = nameList.indexOf(name);
+    if (posicion !== -1) {
+      console.log("El nombre " + name + " está en la posición " +  posicion)
+      return true, posicion;     
+    }
+    else {
+      console.log("El nombre " + name + " no está en el listado")
+      return false;
+    }
+}
+
+nameFinder(names, "Xabier");
